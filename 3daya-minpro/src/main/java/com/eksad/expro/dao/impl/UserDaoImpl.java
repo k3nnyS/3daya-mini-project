d96f42fb13dfe45d6075b8fcbd51e6eb00efd4ab
Mega Aulia Ramadhani <gamssol14@gmail.com>
2019-02-20 12:22:05 -0500

    update daoImpl

--- 3daya-minpro/src/main/java/com/eksad/expro/dao/impl/UserDaoImpl.java
+++ 3daya-minpro/src/main/java/com/eksad/expro/dao/impl/UserDaoImpl.java
@@ -6,10 +6,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.eksad.expro.dao.UserDao;
 import com.eksad.expro.model.UserModel;
 
+@Repository
 public class UserDaoImpl implements UserDao{
 	@Autowired
 	private SessionFactory sessionFactory;
