37281c6565946db20fc5644990e4533ee98efee3
Muchamad Ardiansyah <muchamad.ardiansyah7@gmail.com>
2019-02-20 18:43:00 +0700

    tambah model dao implement

---
+++ 3daya-minpro/src/main/java/com/eksad/expro/dao/TrainerDao.java
@@ -0,0 +1,16 @@
+package com.eksad.expro.dao;
+
+import java.util.List;
+
+import com.eksad.expro.model.TrainerModel;
+
+public interface TrainerDao {
+	
+	public List<TrainerModel> getList();
+	public List<TrainerModel> search(String key);
+	public TrainerModel getById(Integer id);
+	public void insert(TrainerModel model);
+	public void update(TrainerModel model);
+	public void delete(TrainerModel model);
+	
+}
