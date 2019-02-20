06fb8eb505eabe656789cb314e90f44e27812bd4
Muhammad Ishaq <muhammadishaq096@gmail.com>
2019-02-20 23:48:15 +0700

    Benerin UserService

--- 3daya-minpro/src/main/java/com/eksad/expro/service/UserService.java
+++ 3daya-minpro/src/main/java/com/eksad/expro/service/UserService.java
@@ -1,5 +1,16 @@
 package com.eksad.expro.service;
 
+import java.util.List;
+
+import com.eksad.expro.model.UserModel;
+
 public interface UserService {
+	public List<UserModel> getList();
+	public List<UserModel> search(String key);
+	public UserModel getById(Integer id);
+	public void insert(UserModel model);
+	public void update(UserModel model);
+	public void delete(UserModel model);
+	
 
 }
